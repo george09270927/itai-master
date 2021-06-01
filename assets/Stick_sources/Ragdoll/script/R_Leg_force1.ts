ab2a6105c0d942021358d4eabf1b780295fd931e
108000103 <george0927@kimo.com>
2021-06-01 21:14:19 +0800

    add ts and scene Ragdoll2

--- assets/Stick_sources/Ragdoll/script/R_Leg_force1.ts
+++ assets/Stick_sources/Ragdoll/script/R_Leg_force1.ts
@@ -1,45 +1,42 @@
 
 
-cc.Class({
-    extends: cc.Component,
+const {ccclass, property} = cc._decorator;
 
-    properties: {
-        
-     playerSpeed:0,
+@ccclass
+export default class R_Leg_force extends cc.Component {
 
-     zDown: false, // key for player to go left
+    playerSpeed: number =0;
 
-     xDown: false, // key for player to go right
+    zDown: boolean = false; // key for player to go left
 
-     jDown: false, // key for player to shoot
+    xDown: boolean =false; // key for player to go right
 
-     kDown: false, // key for player to jump
+    jDown: boolean =false; // key for player to shoot
 
-     isDead:false,
+    kDown: boolean =false; // key for player to jump
 
-     onGround: false,
-     dDown: false,
-     sDown: false,
+    sDown: boolean = false;
+    dDown: boolean = false;
 
-    },
+    isDead:boolean =false;
+
+    onGround:boolean = false;
 
     onLoad () {
 
-        window.Test_Body = this.node.getComponent(cc.RigidBody);
-
-    },
+    }
 
     
     start() {
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
-    },
+    }
 
 
     update() {
         //cc.log(this.node.position);
         this.playerMovement();
-    },
+    }
 
 
     onKeyDown(event) {
@@ -48,35 +45,35 @@ cc.Class({
         if(event.keyCode == cc.macro.KEY.d) {
             this.dDown = true;
             this.onGround = false;
-        }  else if (event.keyCode == cc.macro.KEY.s) {
+        } else if (event.keyCode == cc.macro.KEY.s) {
             this.sDown = true;
             this.onGround = false;
         }
-    },
+    }
 
     onKeyUp(event) {
         if(event.keyCode == cc.macro.KEY.d)
             this.dDown = false;
         else if (event.keyCode == cc.macro.KEY.s)
             this.sDown = false;
-    },
+    }
     
-    playerMovement(dt) {
+    playerMovement() {
         this.playerSpeed = 0;
         if(this.dDown){
             cc.log("dDown");
-            this.playerSpeed = -150000;
-            this.node.scaleX = -1;
-        } else if (this.sDown) {
-            cc.log("sDown");
             this.playerSpeed = 150000;
+            this.node.scaleX = 1;
+        } else if (this.sDown){
+            cc.log("sDown");
+            this.playerSpeed = -150000;
             this.node.scaleX = 1;
         }
         
         //window.Test_Body.linearVelocity = cc.v2(this.playerSpeed * dt,window.Test_Body.linearVelocity.y)
         //this.node.position.x += this.playerSpeed * dt;
         this.getComponent(cc.RigidBody).applyForceToCenter(new cc.Vec2(this.playerSpeed, 0), true);
-    }  ,
+    }  
     onBeginContact(contact, self, other) {
         var direction = contact.getWorldManifold().normal;
         if (other.node.name == "Ground") {
@@ -84,5 +81,6 @@ cc.Class({
             cc.log("onGround");
         }
         
-    },
-});
+    }
+    
+}
